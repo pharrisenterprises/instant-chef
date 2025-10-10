97e2595cda0d9e6e25fc8a399333cfc8aa49fbfc
MohamedMourad10 <mohamedghaaly.03@gmail.com>
2025-10-10 18:31:18 +0300

    ay haga

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 import DashboardClient from '@/components/DashboardClient';
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export default function Page() {
   return <DashboardClient />;
