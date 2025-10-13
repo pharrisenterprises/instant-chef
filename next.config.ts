b1ae7cfef752297ccb71a90f195ef4d5bcfdc55c
MohamedMourad10 <mohamedghaaly.03@gmail.com>
2025-10-13 15:32:02 +0300

    finale

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,9 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true, // if you also want to skip TS blocking builds
-  },
-};
-
-module.exports = nextConfig;
